10fc351f30a6a9f2f1cbc920ddb3a76fba20a32a
tata <dnfvm132@naver.com>
2024-08-21 23:59:53 +0900

    a

--- 파이썬 코테 17.py
+++ 파이썬 코테 17.py
@@ -2,6 +2,6 @@ a = int(input('초를 입력하세요 :'))
 m = a//60
 s = a%60
 
-print(a,'초는', m,'분', s,'초입니다.')
+print(a,'초(sec)는', m,'분(min)', s,'초(sec)입니다.')
 
 
---
+++ 파이썬 코테 19.py
@@ -0,0 +1,5 @@
+a = ['메이킷','우진','시은']
+print(a[:])
+print(a[0])
+print(a[1])
+print(a[2])
