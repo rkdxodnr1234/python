b46309d7e006950b71ee42ce4ec79c1068bb69ef
tata <dnfvm132@naver.com>
2024-08-24 23:25:53 +0900

    딱 알았어

---
+++ 파이썬 코테 26.py
@@ -0,0 +1,16 @@
+a = [1,2, 3, 4, 5, 'a', 'b', 'c', 'd', 'e']
+
+b = a[:5]
+print('b :',b)
+
+c = a[5:]
+print('c :',c)
+
+d = a[4:6]
+print('d :',d)
+
+e = a[::2]
+print('e :',e)
+
+f = a[1::2]
+print('f :',f)
